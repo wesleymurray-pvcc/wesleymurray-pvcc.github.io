9c3f4a87fa7c771095f7fbc00737878018509605
wesleymurray-pvcc <160061063+wesleymurray-pvcc@users.noreply.github.com>
2024-03-18 13:42:35 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+# Name: Wesley Murray
+# Program Purpose: Hello World in Python
+
+print('Hello World, This is my first Python program.')
+print("I can air quotes program in HTML, which is Awesome!!!")
+print("However, HTML cannot do arithmetic.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("And lots of other things!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
